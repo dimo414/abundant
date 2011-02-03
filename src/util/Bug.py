0416a2cb54a65d96b7e2f699050b13df4380f4dd
Michael Diamond <dimo414@gmail.com>
2011-02-03 02:45:25 -0800

    Exploring JSON functionality in Python, added detailed description of a bug to the wiki

--- src/util/Bug.py
+++ src/util/Bug.py
@@ -7,24 +7,38 @@
 # See http://www.gnu.org/licenses/ for the full license text.
 
 '''
-
 @author: Michael Diamond
 Created on Feb 2, 2011
 '''
 
 import json
 
-class Bug(object):
+class Bug:
     '''
     classdocs
     '''
 
-
-    def __init__(self):
+    def __init__(self,s,hello="Hello world!"):
         '''
         Constructor
         '''
-        
+        self.s = s
+        self.hello = hello
+    def __repr__(self):
+        return '<Bug(%s,%s)>' % (self.s, self.hello)
+
+def bugToJSON(bug):
+    return bug.__dict__
+
+def jSONToBug(json):
+    print(json)
+    args = dict( (key.encode('ascii'), value) for key, value in json.items())
+    print(args)
+    return Bug(**args)
     
 if __name__ == '__main__':
-    print(json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}], sort_keys=True, indent=1))
+    str = json.dumps(Bug("Text",{"a":"aaaa","b":"BBBBB","C":"ccccc"}), default=bugToJSON, sort_keys=True)
+    print(str)
+    print(json.loads(str,object_hook=jSONToBug))
+    
+    
