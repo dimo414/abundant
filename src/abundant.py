1cf87d80c90246a0cb45e57d138e893f4843b6e9
Michael Diamond <dimo414@gmail.com>
2011-02-11 17:12:08 -0800

    Default populated abundant.py

---
+++ src/abundant.py
@@ -0,0 +1,18 @@
+# Copyright 2011 Michael Diamond
+# 
+# This file is part of Abundant.
+# 
+# This software may be used and distributed according to the terms of
+# the  GNU General Public License version 3 or any later version.
+# See http://www.gnu.org/licenses/ for the full license text.
+
+'''
+Main module, handles parsing the CLI arguments, loading config
+data, and passing work off to commands
+
+@author: Michael Diamond
+Created on Feb 10, 2011
+'''
+
+if __name__ == '__main__':
+    pass
